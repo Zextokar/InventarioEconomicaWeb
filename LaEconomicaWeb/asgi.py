30e376abf139b4b54522c6c0701e607e9312966f
Zextokar <rrodriguez.rrg28@gmail.com>
2023-12-17 22:56:13 -0300

    NewBe

--- LaEconomicaWeb/asgi.py
+++ LaEconomicaWeb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for inventarioVeterinariaRenatoYBenjamin project.
+ASGI config for LaEconomicaWeb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventarioVeterinariaRenatoYBenjamin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LaEconomicaWeb.settings')
 
 application = get_asgi_application()
